e513a507127d3ac1a22a6dfb9f67193d8e31704d
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-25 13:25:55 -0800

    modify FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,8 @@
 """
 Contains the FileStorage class
 """
+import json
+from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -13,3 +15,28 @@ class FileStorage:
     def all(self):
         """returns the dictionary __objects"""
         return self.__objects
+
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id"""
+        ind = obj.__class__.__name__ + '.' + obj.id
+        self.__objects[ind] = obj
+
+    def save(self):
+        """ serializes __objects to the JSON file"""
+        diction = {}
+        for x in self.__objects:
+            diction[x] = self.__objects[x].to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(diction, f)
+
+    def reload(self):
+        """  deserializes the JSON file to __objects """
+        try:
+            with open(self.__file_path, "r") as f:
+                jo = json.load(f)
+            for o in jo.values():
+                cls_name = o["__class__"]
+                del o["__class__"]
+                self.new(eval(cls_name)(**o))
+        except:
+            pass
