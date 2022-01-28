06f3f4eac7d5bc0e35516dd3d2e49cbfd8afb71b
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-28 09:36:18 -0800

    add comment

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
 #!usr/bin/python3
 """
-
+File Storage class
 """
 
 
@@ -13,7 +13,8 @@ classes = {"BaseModel": BaseModel}
 
 class FileStorage:
     """
-
+    serializes instances to a JSON file and deserializes 
+    JSON file to instances:
     """
     __file_path = "file.json"
     __objects = {}
