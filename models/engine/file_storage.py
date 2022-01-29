ea41e157fbe0d4b50b8e53b21bba96973f11490a
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-28 21:21:14 -0800

    merge

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!usr/bin/python3
 """
 File Storage class
@@ -13,14 +12,14 @@ classes = {"BaseModel": BaseModel}
 
 class FileStorage:
     """
-    serializes instances to a JSON file and deserializes 
+    serializes instances to a JSON file and deserializes
     JSON file to instances:
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-<<<<<<< HEAD
+        """returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
@@ -28,11 +27,11 @@ class FileStorage:
         self.__objects[ind] = obj
 
     def save(self):
-         """serializes __objects to the JSON file (path: __file_path)"""
-         json_objects = {}
-         for key in self.__objects:
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_objects = {}
+        for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
-         with open(self.__file_path, 'w') as f:
+        with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
     def reload(self):
@@ -42,32 +41,5 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-=======
-        """returns the dictionary __objects"""
-        return self.__objects
-
-    def new(self, obj):
-        """ sets in __objects the obj with key <obj class name>.id"""
-        ind = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[ind] = obj
-
-    def save(self):
-        """ serializes __objects to the JSON file"""
-        diction = {}
-        for x in self.__objects:
-            diction[x] = self.__objects[x].to_dict()
-        with open(self.__file_path, "w") as f:
-            json.dump(diction, f)
-
-    def reload(self):
-        """  deserializes the JSON file to __objects """
-        try:
-            with open(self.__file_path, "r") as f:
-                jo = json.load(f)
-            for o in jo.values():
-                cls_name = o["__class__"]
-                del o["__class__"]
-                self.new(eval(cls_name)(**o))
->>>>>>> 6c99573bd458e64cd849b3d22e022e063d160b9d
-        except:
+        except Exception:
             pass
