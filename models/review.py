7d542d7dda8c33754d0da87e4b55d70178841642
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-29 00:12:11 -0800

    add review class

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!usr/bin/pyhton3
+"""
+Review class
+"""
+from models.base_model import BaseModel
+
+
+class Review:
+    """describe Review """
+    place_id = ""
+    user_id = ""
+    text = ""
